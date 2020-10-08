fe048c16f571c985aa8f4cdc2c209a0df0d44a70
NateWhitehead96 <n_whitehead96@hotmail.ca>
2020-10-08 11:21:27 -0400

    Setting up assets and packages
    Created base for assets, needed packages and started on overworld map

---
+++ GAME3023_FinalProject/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Easy movement using Axis Input
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 movementVector = new Vector2(x, y);
+        movementVector *= speed;
+        rigidbody.velocity = movementVector; // Now using rigidbody for movement
+    }
+}
